73d7f6c924ab0f66c0cf8e2714c9aa6a7eb74be5
René Moser <mail@renemoser.net>
2026-09-09 17:18:34 +0200

    new upstream 4.23.0.0

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash +x
+set -euo pipefail
+export PATH="/root/.local/bin:$PATH"
 
-/usr/bin/mysqld_safe &
-cd /opt/cloudstack && mvn -pl client jetty:run -Dsimulator -Dorg.eclipse.jetty.annotations.maxWait=120 &
+# Run the CloudStack Simulator
+/usr/bin/supervisord --silent &
 
+echo "--- Waiting for CloudStack Simulator to start ---"
+sleep 30
 until nc -z localhost 8096; do
-    echo "waiting for port 8096..."
+    echo "--- ...waiting for port 8096... ---"
     sleep 3
 done
-
-sleep 3
-python3 /opt/cloudstack/tools/marvin/marvin/deployDataCenter.py -i /opt/zones.cfg
+sleep 10
+echo "--- Deploying CloudStack Simulator Zones (this will take some minutes...) ---"
+python3 /root/tools/marvin/marvin/deployDataCenter.py -i /opt/zones.cfg
 
 export CLOUDSTACK_ENDPOINT=http://127.0.0.1:8096
 export CLOUDSTACK_KEY=dummy
@@ -17,3 +21,8 @@ export CLOUDSTACK_SECRET=dummy
 
 # Add Simulator to supported hypervisors exclusively
 cs updateConfiguration name=hypervisor.list value=Simulator
+
+# Stop the CloudStack Simulator
+supervisorctl stop all
+
+echo "--- CloudStack Simulator Zones deployed ---"
